eff5b7f8f147f700832f3b6bcab429c6075dde9f
patrick Essomba <patrickassako78@gmail.com>
2022-08-15 13:09:28 +0200

    Ajout du projet complet

---
+++ android/app/src/main/kotlin/com/example/upgradeapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.upgradeapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,138 +15,151 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[150],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.account_balance_wallet_rounded,
-                size: 100,
-                color: Colors.deepPurple[700],
-              ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.account_balance_wallet_rounded,
+                  size: 100,
+                  color: Colors.deepPurple[700],
+                ),
 
-              //Text d'acceuil
+                //Text d'acceuil
 
-              SizedBox(height: 40),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Row(
-                  children: [
-                    Text(
-                      "Bienvenu sur ",
-                      style: TextStyle(
-                        fontSize: 34,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text("My Finances",
-                        style: GoogleFonts.poppins(
+                SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Bienvenu sur ",
+                        style: TextStyle(
                           fontSize: 30,
-                          color: Colors.deepPurple,
-                        )),
-                  ],
-                ),
-              ),
-              SizedBox(height: 15),
-              //Description
-              Text(
-                "Heureux de vous revoir ",
-                style: TextStyle(
-                  fontSize: 23,
-                ),
-              ),
-              SizedBox(height: 40),
-              // Nom d'utilisateur
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.deepPurpleAccent),
-                    borderRadius: BorderRadius.circular(15),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text("My Finances",
+                          style: GoogleFonts.poppins(
+                            fontSize: 30,
+                            color: Colors.deepPurple,
+                          )),
+                    ],
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Nom d'Utilisateur",
+                ),
+                SizedBox(height: 15),
+                //Description
+                Text(
+                  "Heureux de vous revoir ",
+                  style: TextStyle(
+                    fontSize: 23,
+                  ),
+                ),
+                SizedBox(height: 40),
+                // Nom d'utilisateur
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.deepPurpleAccent),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Nom d'Utilisateur",
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-              //Mot de passe
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.blue),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Mot de passe",
+                SizedBox(height: 10),
+                //Mot de passe
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Mot de passe",
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              //Bouton validation
-              SizedBox(height: 10),
+                //Bouton validation
+                SizedBox(height: 10),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  padding: EdgeInsets.all(25.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.deepPurple,
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Se connecter",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    padding: EdgeInsets.all(25.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.deepPurple,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Se connecter",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-              // pas enregistré? enregistrez-vous maintenant
+                SizedBox(height: 30),
+                // pas enregistré? enregistrez-vous maintenant
 
-              Padding(
-                padding: const EdgeInsets.only(left: 100.0),
-                child: Row(
-                  children: [
-                    Text(
-                      'pas de compte?',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                Padding(
+                  padding: const EdgeInsets.only(left: 100.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        'pas de compte?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      "Créez un compte",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
+                      SizedBox(width: 10),
+                      Text(
+                        "Créez un compte",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 120),
+                Padding(
+                  padding: const EdgeInsets.only(left: 250),
+                  child: Text(
+                    "By UPGRADE",
+                    style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      color: Colors.blue[500],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
